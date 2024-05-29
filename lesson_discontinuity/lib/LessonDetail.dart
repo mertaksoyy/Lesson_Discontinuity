ef75ea8f4d0efe84a4da44b922ffb151d1013959
Mert Aksoy <mrtaksy99@gmail.com>
2024-05-29 17:18:16 +0300

    cocapods

--- lesson_discontinuity/lib/LessonDetail.dart
+++ lesson_discontinuity/lib/LessonDetail.dart
@@ -1,25 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:lesson_discontinuity/DbModel/dbDao.dart';
-import 'package:lesson_discontinuity/Lessons.dart';
+import 'package:lesson_discontinuity/data/entity/Lessons.dart';
 import 'package:lesson_discontinuity/main.dart';
 
 class LessonDetail extends StatefulWidget {
-  const LessonDetail({super.key,required this.lesson});
-  final Lessons  lesson;
+  const LessonDetail({super.key, required this.lesson});
+  final Lessons lesson;
   @override
   State<LessonDetail> createState() => _lessonDetailState();
 }
 
 class _lessonDetailState extends State<LessonDetail> {
-
-
   int discontinuity = 0;
   TextEditingController tfDetailCode = TextEditingController();
   TextEditingController tfDetailDiscontinuity = TextEditingController();
 
-  Future<void> lessonUpdate(int lessonId,String lessonCode,int lessonDiscontinuity ) async{
+  Future<void> lessonUpdate(
+      int lessonId, String lessonCode, int lessonDiscontinuity) async {
     await dbDao().updateLesson(lessonId, lessonCode, lessonDiscontinuity);
-    Navigator.push(context, MaterialPageRoute(builder: (context) => MyHomePage()));
+    Navigator.push(
+        context, MaterialPageRoute(builder: (context) => MyHomePage()));
   }
 
   @override
@@ -34,8 +34,9 @@ class _lessonDetailState extends State<LessonDetail> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Lesson Update"),),
-
+      appBar: AppBar(
+        title: const Text("Lesson Update"),
+      ),
       body: Center(
         child: Padding(
           padding: const EdgeInsets.all(25.0),
@@ -44,21 +45,25 @@ class _lessonDetailState extends State<LessonDetail> {
             children: [
               TextField(
                 controller: tfDetailCode,
-                decoration: const InputDecoration(labelText: "Enter Lesson Code",),
+                decoration: const InputDecoration(
+                  labelText: "Enter Lesson Code",
+                ),
               ),
               TextField(
                 controller: tfDetailDiscontinuity,
-                decoration: const InputDecoration(labelText: "Enter Discontinuity",),
+                decoration: const InputDecoration(
+                  labelText: "Enter Discontinuity",
+                ),
               ),
             ],
           ),
         ),
       ),
-      floatingActionButton:FloatingActionButton.extended(
-        icon:Icon(Icons.update),
-        label: Text("Update"),
+      floatingActionButton: FloatingActionButton.extended(
+        icon: const Icon(Icons.update),
+        label: const Text("Update"),
         tooltip: "Update",
-        onPressed: (){
+        onPressed: () {
           discontinuity = int.parse(tfDetailDiscontinuity.text);
           lessonUpdate(widget.lesson.ders_id, tfDetailCode.text, discontinuity);
         },
--- lesson_discontinuity/lib/main.dart
+++ lesson_discontinuity/lib/main.dart
@@ -31,17 +31,14 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
-
-  Future<List<Lessons>> getAll() async{
+  Future<List<Lessons>> getAll() async {
     var lessonList = await dbDao().allLesson();
     return lessonList;
   }
 
-  Future<void> delete(int lesson_id) async{
+  Future<void> delete(int lesson_id) async {
     await dbDao().removeLesson(lesson_id);
-    setState(() {
-    });
+    setState(() {});
   }
 
   @override
@@ -50,30 +47,42 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: const Text("Lesson Discontinuity App"),
       ),
-      body:FutureBuilder<List<Lessons>>(
+      body: FutureBuilder<List<Lessons>>(
         future: getAll(),
-        builder: (context,snapshot){
-          if(snapshot.hasData){
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
             var getList = snapshot.data;
             return ListView.builder(
                 itemCount: getList!.length,
-                itemBuilder: (context,indeks){
+                itemBuilder: (context, indeks) {
                   var list = getList[indeks];
                   return GestureDetector(
-                    onTap: (){
-                      Navigator.push(context, MaterialPageRoute(builder: (context) => LessonDetail(lesson: list,)));
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => LessonDetail(
+                                    lesson: list,
+                                  )));
                     },
-                    child:Card(
-                      child: SizedBox(height: 60,
+                    child: Card(
+                      child: SizedBox(
+                        height: 60,
                         child: Row(
                           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                           children: [
                             //Text(list.ders_id.toString()),
-                            Text(list.ders_ad,style: TextStyle(fontWeight: FontWeight.bold),),
-                            Text(list.ders_devamsizlik.toString(),style: TextStyle(fontWeight: FontWeight.bold),),
+                            Text(
+                              list.ders_ad,
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                            Text(
+                              list.ders_devamsizlik.toString(),
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
                             IconButton(
                               icon: Icon(Icons.delete),
-                              onPressed: (){
+                              onPressed: () {
                                 delete(list.ders_id);
                               },
                             )
@@ -82,22 +91,21 @@ class _MyHomePageState extends State<MyHomePage> {
                       ),
                     ),
                   );
-                }
-            );
-          }else{
+                });
+          } else {
             return const Center();
           }
         },
       ),
-      floatingActionButton:FloatingActionButton.extended(
-        icon:Icon(Icons.add),
+      floatingActionButton: FloatingActionButton.extended(
+        icon: Icon(Icons.add),
         label: Text("Add"),
         tooltip: "Add",
-        onPressed: (){
-          Navigator.push(context, MaterialPageRoute(builder: (context) =>  AddLesson()));
+        onPressed: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => AddLesson()));
         },
       ),
     );
   }
 }
-
